ea7361c4e7d14ea7db66d018a33d81fdf08f3577
suhang <suhang@roobo.com>
2020-07-22 16:53:10 +0800

    test

--- app/src/main/java/com/insightsurfface/demodemo/business/test/TestActivity1.java
+++ app/src/main/java/com/insightsurfface/demodemo/business/test/TestActivity1.java
@@ -1,10 +1,13 @@
 package com.insightsurfface.demodemo.business.test;
 
+import android.Manifest;
 import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,25 +15,70 @@ import android.widget.TextView;
 import com.insightsurfface.demodemo.R;
 import com.insightsurfface.demodemo.base.BaseActivity;
 import com.insightsurfface.demodemo.utils.DisplayUtil;
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+
+import java.io.File;
+import java.util.List;
 
 import androidx.annotation.Nullable;
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
 
 /**
  * Created by acorn on 2020/7/11.
  */
-public class TestActivity1 extends BaseActivity {
-    private ImageView watchHandIv;
-    private ObjectAnimator rotation;
+public class TestActivity1 extends BaseActivity implements EasyPermissions.PermissionCallbacks {
+    private static String TAG = "TestActivity1";
+    private TextView default_prompt;
+    private ImageView waiting_iv;
+    private boolean ret;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initAnim();
+        unzipFile();
+        default_prompt.setText(ret + "");
+        ImageLoader.getInstance().displayImage("file://" + Environment
+                .getExternalStorageDirectory().getAbsolutePath() + File.separator + "aSpider" +
+                File.separator + "Pluto" + File.separator + "1" + File.separator + "manga_1_0.png", waiting_iv);
     }
 
     @Override
     protected void initUI() {
-        watchHandIv = findViewById(R.id.watch_hand_iv);
+        default_prompt = findViewById(R.id.default_prompt);
+        waiting_iv = findViewById(R.id.waiting_iv);
+    }
+
+    @AfterPermissionGranted(111)
+    private void unzipFile() {
+        String[] perms = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
+        if (EasyPermissions.hasPermissions(this, perms)) {
+            // Already have permission, do the thing
+            // ...
+            Log.d(TAG, "unzipFile() called");
+            boolean ret = true;
+            File file = new File(Environment
+                    .getExternalStorageDirectory().getAbsolutePath() + File.separator + "aSpider" + File.separator + "dist.zip");
+            if (file.exists()) {
+                ZipFile zipFile;
+                try {
+                    zipFile = new ZipFile(file);
+                    zipFile.extractAll(Environment
+                            .getExternalStorageDirectory().getAbsolutePath() + File.separator + "aSpider");
+                } catch (ZipException e) {
+                    e.printStackTrace();
+                    ret = false;
+                }
+//                boolean delete = file.delete();
+            }
+        } else {
+            // Do not have permissions, request them now
+            EasyPermissions.requestPermissions(this, "我们需要写入/读取权限",
+                    111, perms);
+        }
     }
 
     @Override
@@ -38,21 +86,26 @@ public class TestActivity1 extends BaseActivity {
         return R.layout.activity_waiting;
     }
 
-    private void initAnim() {
-        //表针的宽高 因为是固定值直接写死
-        watchHandIv.setPivotX(DisplayUtil.dip2px(this,8));
-        watchHandIv.setPivotY(DisplayUtil.dip2px(this,29.7f));
-        rotation = ObjectAnimator.ofFloat(watchHandIv, "rotation", 0f, 360f);
-        rotation.setRepeatCount(ValueAnimator.INFINITE);
-        rotation.setRepeatMode(ValueAnimator.RESTART);
-        rotation.setInterpolator(new LinearInterpolator());
-        rotation.setDuration(800);
-        rotation.start();
-    }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        rotation.cancel();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // Forward results to EasyPermissions
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+        baseToast.showToast("已获得授权,请继续!");
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
     }
 }
