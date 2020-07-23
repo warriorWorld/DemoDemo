5f1dc7e50db2c5dab317608a1588f8bc3079a7ec
suhang <suhang@roobo.com>
2020-07-23 14:30:22 +0800

    7z

--- app/src/main/java/com/insightsurfface/demodemo/business/test/TestActivity1.java
+++ app/src/main/java/com/insightsurfface/demodemo/business/test/TestActivity1.java
@@ -1,36 +1,23 @@
 package com.insightsurfface.demodemo.business.test;
 
-import android.Manifest;
-import android.animation.Animator;
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
-import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
-import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.hzy.libp7zip.P7ZipApi;
 import com.insightsurfface.demodemo.R;
 import com.insightsurfface.demodemo.base.BaseActivity;
-import com.insightsurfface.demodemo.utils.DisplayUtil;
-import com.nostra13.universalimageloader.core.ImageLoader;
-
-import net.lingala.zip4j.core.ZipFile;
-import net.lingala.zip4j.exception.ZipException;
 
 import java.io.File;
-import java.util.List;
 
 import androidx.annotation.Nullable;
-import pub.devrel.easypermissions.AfterPermissionGranted;
-import pub.devrel.easypermissions.EasyPermissions;
 
 /**
  * Created by acorn on 2020/7/11.
  */
-public class TestActivity1 extends BaseActivity implements EasyPermissions.PermissionCallbacks {
+public class TestActivity1 extends BaseActivity {
     private static String TAG = "TestActivity1";
     private TextView default_prompt;
     private ImageView waiting_iv;
@@ -41,9 +28,6 @@ public class TestActivity1 extends BaseActivity implements EasyPermissions.Permi
         super.onCreate(savedInstanceState);
         unzipFile();
         default_prompt.setText(ret + "");
-        ImageLoader.getInstance().displayImage("file://" + Environment
-                .getExternalStorageDirectory().getAbsolutePath() + File.separator + "aSpider" +
-                File.separator + "Pluto" + File.separator + "1" + File.separator + "manga_1_0.png", waiting_iv);
     }
 
     @Override
@@ -52,33 +36,21 @@ public class TestActivity1 extends BaseActivity implements EasyPermissions.Permi
         waiting_iv = findViewById(R.id.waiting_iv);
     }
 
-    @AfterPermissionGranted(111)
     private void unzipFile() {
-        String[] perms = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
-        if (EasyPermissions.hasPermissions(this, perms)) {
-            // Already have permission, do the thing
-            // ...
-            Log.d(TAG, "unzipFile() called");
-             ret = true;
-            File file = new File(Environment
-                    .getExternalStorageDirectory().getAbsolutePath() + File.separator + "aSpider" + File.separator + "dist.zip");
-            if (file.exists()) {
-                ZipFile zipFile;
-                try {
-                    zipFile = new ZipFile(file);
-                    zipFile.extractAll(Environment
-                            .getExternalStorageDirectory().getAbsolutePath() + File.separator + "aSpider");
-                } catch (ZipException e) {
-                    e.printStackTrace();
-                    ret = false;
-                }
-//                boolean delete = file.delete();
-            }
-        } else {
-            // Do not have permissions, request them now
-            EasyPermissions.requestPermissions(this, "我们需要写入/读取权限",
-                    111, perms);
+        Log.d(TAG, "unzipFile() called");
+        ret = true;
+        File file = new File(Environment
+                .getExternalStorageDirectory().getAbsolutePath() + File.separator + "aSpider" + File.separator + "dist.zip");
+        File outFile = new File(Environment
+                .getExternalStorageDirectory().getAbsolutePath() + File.separator + "aSpider");
+        if (file.exists() && outFile.exists()) {
+            P7ZipApi.executeCommand(getExtractCmd(file.getPath(), outFile.getPath()));
+//            boolean delete = file.delete();
         }
+    }
+
+    public static String getExtractCmd(String archivePath, String outPath) {
+        return String.format("7z x '%s' '-o%s' -aoa", archivePath, outPath);
     }
 
     @Override
@@ -90,22 +62,5 @@ public class TestActivity1 extends BaseActivity implements EasyPermissions.Permi
     @Override
     protected void onDestroy() {
         super.onDestroy();
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                                           int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        // Forward results to EasyPermissions
-        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
-    }
-
-    @Override
-    public void onPermissionsGranted(int requestCode, List<String> perms) {
-        baseToast.showToast("已获得授权,请继续!");
-    }
-
-    @Override
-    public void onPermissionsDenied(int requestCode, List<String> perms) {
     }
 }
